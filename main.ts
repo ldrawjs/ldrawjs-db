4b8de0293c8a8e3e6a01a837d1d5a3d1d34a1111
Ziv <ziv.perry@zoominfo.com>
2023-02-12 16:09:59 +0200

    done

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { walk } from "https://deno.land/std@0.177.0/fs/walk.ts";
+import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 
 async function find(name: string) {
     for await (const e of walk("./l")) {
@@ -9,6 +10,22 @@ async function find(name: string) {
     return "";
 }
 
+const headers = {
+    "content-type": "text/plain",
+    "access-control-allow-origin": "*",
+};
 
+async function handler(req: Request) {
+    const file = (new URL(req.url).pathname).substring(1);
+    const path = await find(file);
+    if ("" === path) {
+        return new Response("not found", {
+            status: 404,
+            headers
+        });
+    }
+    const ldr = await Deno.readTextFile(path);
+    return new Response(ldr, {headers})
+}
 
-console.log(await find("3005.dat"));
+await serve(handler);
